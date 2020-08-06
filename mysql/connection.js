c0ce2c93e92628fb942bb60361cf1629c6d191b7
WilliamRHarper <Wrharpe1230@gmail.com>
2020-08-05 20:02:45 -0500

    added express, mysql, connections, pool, dotenv, router

---
+++ mysql/connection.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+require('dotenv').config();
+
+class Connection {
+    constructor() {
+      if (!this.pool) {
+        console.log("creating connection...");
+        this.pool = mysql.createPool({
+          connectionLimit: 100,
+          host: "127.0.0.1",
+          user: "root",
+          password: process.env.DB_PASSWORD,
+          database: "admin",
+        });
+  
+        return this.pool;
+      }
+  
+      return this.pool;
+    }
+  }
+  
+  const instance = new Connection();
+  
+  module.exports = instance;
---
+++ routes/employees.js
@@ -0,0 +1,3 @@
+const express = require('express')
+const usersController = require('../controllers/users')
+const router = express.Router()
